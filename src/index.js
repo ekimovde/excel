dcef77fe4c429360dc69ee4893eb9112e939c77e
ekimovde <61349359+ekimovde@users.noreply.github.com>
2021-03-06 19:54:51 +0700

    Finish base framework functional

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-import './scss/index.scss';
+import { Excel } from "./components/excel/Excel";
+import { Header } from "@/components/header/Header";
+import { Toolbar } from "@/components/toolbar/Toolbar";
+import { Formula } from "@/components/formula/Formula";
+import { Tables } from "@/components/table/Tables";
 
-console.log('Hello world!');
+import "./scss/index.scss";
+
+const excel = new Excel("#app", {
+  components: [Header, Toolbar, Formula, Tables],
+});
+
+excel.render();
--- webpack.config.js
+++ webpack.config.js
@@ -1,89 +1,93 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const ESLintPlugin = require('eslint-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+// const ESLintPlugin = require('eslint-webpack-plugin')
 
 module.exports = (env, argv) => {
-  const isProd = argv.mode === 'production'
-  const isDev = !isProd
+  const isProd = argv.mode === "production";
+  const isDev = !isProd;
 
   const filename = (ext) =>
-    isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`
+    isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`;
+
+  const jsLoaders = () => {
+    const loaders = [
+      {
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"],
+          plugins: ["@babel/plugin-proposal-class-properties"],
+        },
+      },
+    ];
+
+    return loaders;
+  };
 
   const plugins = () => {
     const base = [
       new HtmlWebpackPlugin({
-        template: './index.html'
+        template: "./index.html",
       }),
       new CopyPlugin({
         patterns: [
           {
-            from: path.resolve(__dirname, 'src', 'favicon.ico'),
-            to: path.resolve(__dirname, 'dist')
+            from: path.resolve(__dirname, "src", "favicon.ico"),
+            to: path.resolve(__dirname, "dist"),
           },
         ],
       }),
       new MiniCssExtractPlugin({
-        filename: filename('css')
+        filename: filename("css"),
       }),
       new CleanWebpackPlugin(),
-    ]
+    ];
 
-    if (isDev) {
-      base.push(new ESLintPlugin())
-    }
+    // if (isDev) {
+    //   base.push(new ESLintPlugin())
+    // }
 
-    return base
-  }
+    return base;
+  };
 
   return {
-    target: 'web',
-    context: path.resolve(__dirname, 'src'),
+    target: "web",
+    context: path.resolve(__dirname, "src"),
     entry: {
-      main: './index.js'
+      main: "./index.js",
     },
     output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: filename('js')
+      path: path.resolve(__dirname, "dist"),
+      filename: filename("js"),
     },
     resolve: {
-      extensions: ['.js'],
+      extensions: [".js"],
       alias: {
-        '@': path.resolve(__dirname, 'src'),
-        '@core': path.resolve(__dirname, 'src', 'core'),
-      }
+        "@": path.resolve(__dirname, "src"),
+        "@core": path.resolve(__dirname, "src", "core"),
+      },
     },
     devServer: {
-      port: '8080',
+      port: "8080",
       open: true,
       hot: true,
-      watchContentBase: true
     },
-    devtool: isDev ? 'source-map' : false,
+    devtool: isDev ? "source-map" : false,
     plugins: plugins(),
     module: {
       rules: [
         {
           test: /\.s[ac]ss$/i,
-          use: [
-            MiniCssExtractPlugin.loader,
-            'css-loader',
-            'sass-loader',
-          ],
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env']
-            }
-          }
-        }
+          use: jsLoaders(),
+        },
       ],
-    }
-  }
-}
+    },
+  };
+};
