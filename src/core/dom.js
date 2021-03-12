bc53356640b637989cd602e0ef860f5483acccf3
ekimovde <61349359+ekimovde@users.noreply.github.com>
2021-03-12 21:15:02 +0700

    Finish base table logic

--- src/core/dom.js
+++ src/core/dom.js
@@ -15,6 +15,18 @@ class Dom {
     return this.$el.outerHTML.trim();
   }
 
+  text(text) {
+    if (typeof text === "string") {
+      this.$el.textContent = text;
+      return this;
+    } else {
+      if (this.$el.tagName.toLowerCase() === "input") {
+        return this.$el.value.trim();
+      }
+      return this.$el.textContent.trim();
+    }
+  }
+
   clear() {
     this.html("");
     return this;
@@ -26,6 +38,38 @@ class Dom {
 
   off(eventType, callback) {
     this.$el.removeEventListener(eventType, callback);
+  }
+
+  find(selector) {
+    return $(this.$el.querySelector(selector));
+  }
+
+  addClass(className) {
+    this.$el.classList.add(className);
+    return this;
+  }
+
+  removeClass(className) {
+    this.$el.classList.remove(className);
+    return this;
+  }
+
+  id(parse) {
+    if (parse) {
+      const parsed = this.id().split(":");
+
+      return {
+        row: +parsed[0],
+        col: +parsed[1],
+      };
+    }
+    return this.data.id;
+  }
+
+  focus() {
+    this.$el.focus();
+
+    return this;
   }
 
   append(node) {
